86f6551af9f0780ef9189b5293b35f060b59fc10
冨澤和也 <k9691.uzaykazu@gmail.com>
2020-04-26 22:07:10 +0900

    処理を構造化

--- SwapPoint-Rakuten.py
+++ SwapPoint-Rakuten.py
@@ -1,10 +1,12 @@
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
 
-html=requests.get("https://www.rakuten-sec.co.jp/web/fx/RateData/SwapData.dat").text
-soup=BeautifulSoup(html,"html.parser")
-print(soup.prettify)
+def text_organizer(swap_text):
+    print(swap_text)
 
+if __name__ == "__main__":
+    html=requests.get("https://www.rakuten-sec.co.jp/web/fx/RateData/SwapData.dat").text
+    soup=BeautifulSoup(html,"html.parser")
 
-
+    swap_text = soup.prettify
+   
